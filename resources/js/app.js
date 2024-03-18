338190f9ca4a5377f9f6685bbb9ebfe0701c7a62
Kamran\kam <hakimikamranullah@gmail.com>
2024-03-18 22:44:27 +0430

    add aliases and make changes in design

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,21 @@
 import "./bootstrap";
 import { createApp } from "vue";
-import App from "./src/App.vue";
-import router from "./src/router";
-import "./src/assets/main.css";
+import App from "@/App.vue";
+import router from "@/router";
+import "@/assets/main.css";
+import PrimeVue from 'primevue/config';
+import DashboardLayout from "@/components/DashboardLayout.vue";
+import EmptyLayout from "@/components/EmptyLayout.vue";
 
-import DashboardLayout from "./src/components/DashboardLayout.vue";
-import EmptyLayout from "./src/components/EmptyLayout.vue";
+//in main.js
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
 
 const app = createApp(App);
 
 app.component("DefaultLayout", DashboardLayout);
 app.component("EmptyLayout", EmptyLayout);
 
+app.use(PrimeVue);
 app.use(router);
 app.mount("#app");
