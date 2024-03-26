c3f86980c4871bbddde900c216cf1d0f8daa6940
Kamran\kam <hakimikamranullah@gmail.com>
2024-03-26 22:47:02 +0430

    change user icon

--- resources/js/src/components/Sidebar.vue
+++ resources/js/src/components/Sidebar.vue
@@ -49,8 +49,8 @@ const inactiveClass = ref(
                         />
                     </svg>
 
-                    <span class="mx-2 text-2xl font-semibold text-white"
-                        >V-Dashboard</span
+                    <span class="mx-2 text-xl font-semibold text-white"
+                        >SMS-Dashboard</span
                     >
                 </div>
             </div>
@@ -85,36 +85,22 @@ const inactiveClass = ref(
                 </router-link>
 
                 <router-link
-                    class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
+                    class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4 profile"
                     :class="[
                         $route.name === 'Profile' ? activeClass : inactiveClass,
                     ]"
                     to="/profile"
                 >
                     <svg
-                        class="w-5 h-5"
-                        viewBox="0 0 20 20"
-                        fill="none"
+                        class="w-5 h-5 user-icon"
                         xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 496 512"
                     >
                         <path
-                            d="M5 3C3.89543 3 3 3.89543 3 5V7C3 8.10457 3.89543 9 5 9H7C8.10457 9 9 8.10457 9 7V5C9 3.89543 8.10457 3 7 3H5Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M5 11C3.89543 11 3 11.8954 3 13V15C3 16.1046 3.89543 17 5 17H7C8.10457 17 9 16.1046 9 15V13C9 11.8954 8.10457 11 7 11H5Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M11 5C11 3.89543 11.8954 3 13 3H15C16.1046 3 17 3.89543 17 5V7C17 8.10457 16.1046 9 15 9H13C11.8954 9 11 8.10457 11 7V5Z"
-                            fill="currentColor"
-                        />
-                        <path
-                            d="M11 13C11 11.8954 11.8954 11 13 11H15C16.1046 11 17 11.8954 17 13V15C17 16.1046 16.1046 17 15 17H13C11.8954 17 11 16.1046 11 15V13Z"
+                            d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8 .4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"
                             fill="currentColor"
                         />
                     </svg>
-
                     <span class="mx-4">Profile</span>
                 </router-link>
                 <router-link
@@ -282,3 +268,10 @@ const inactiveClass = ref(
         </div>
     </div>
 </template>
+
+<style>
+.user-icon {
+    fill: gray;
+    transition: fill 0.3s;
+}
+</style>
