c66573afd5e893f260dd4765cff805fb33ee692b
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-20 22:30:17 +0430

    work on attendance

---
+++ resources/js/src/composables/useAttendance.js
@@ -0,0 +1,26 @@
+import { ref } from "vue";
+import axios from "axios";
+import { useLogin } from "@/composables/user/useLogin";
+
+export const useAttendance = () => {
+    const token = ref("");
+    const students = ref("");
+    const { getCookie, removeCookie } = useLogin();
+    const index = async () => {
+        token.value = getCookie("access_token");
+        await axios
+            .get("/api/student/attendance", {
+                headers: {
+                    Authorization: `Bearer ${token.value}`,
+                },
+            })
+            .then((res) => {
+                students.value = res.data;
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    return { index, students };
+};
--- resources/js/src/views/attendance/index.vue
+++ resources/js/src/views/attendance/index.vue
@@ -1,10 +1,40 @@
 <template>
-    <button>Take Attendance</button>
+    <div class="flex flex-wrap" v-if="students.length > 0">
+        <div
+            v-for="student in students"
+            class="mx-4 bg-white p-4 flex flex-col items-center gap-2 justify-center"
+        >
+            <img
+                :src="`/storage/${student.photo}`"
+                class="w-[80px] h-[80px] rounded-full"
+                alt=""
+            />
+
+            <h5>{{ student.name }}</h5>
+            <div>
+                <label for="present">
+                    <input type="radio" name="attendance" id="present" />
+                    <span>Present</span>
+                </label>
+                <label for="obsent">
+                    <input type="radio" name="attendance" id="obsent" />
+                    <span>Obsent</span>
+                </label>
+                <label for="leave">
+                    <input type="radio" name="attendance" id="leave" />
+                    <span>Leave</span>
+                </label>
+            </div>
+        </div>
+    </div>
 </template>
 <script setup>
 import { onMounted } from "vue";
+import { useAttendance } from "@/composables/useAttendance";
+
+const { index, students } = useAttendance();
 
 onMounted(() => {
-    
+    index();
 });
 </script>
