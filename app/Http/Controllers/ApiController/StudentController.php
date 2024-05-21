039a8e8e21ffb8941380acd568c1a09e154efa3c
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-21 18:34:01 +0430

    take attendance

--- app/Http/Controllers/ApiController/StudentController.php
+++ app/Http/Controllers/ApiController/StudentController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StudentRegisterRequest;
 use App\Http\Requests\StudentUpdateRequest;
-use App\Models\Attendance;
-use App\Models\Classes;
 use App\Models\Student;
 use Illuminate\Support\Facades\Storage;
 
