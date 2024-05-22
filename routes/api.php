8016b46d344e910b1d6e7ebda1dbc5475cb36abd
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-22 06:59:02 +0430

    make new route

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,7 @@ Route::middleware('auth:sanctum', 'role:Admin|Student|Teacher|Parent')->group(fu
     Route::post('/student/update', [StudentController::class, 'update']);
     Route::get('/student/attendance', [AttendanceController::class, 'getClassStudent']);
     Route::post('/attendance/take', [AttendanceController::class, 'takeAttendance']);
+    Route::get('/attendance/class/daily_report', [AttendanceController::class, 'getClassDailyReport']);
 
     Route::get('/teachers/index', [TeacherController::class, 'index']);
     Route::post('/teacher/add', [TeacherController::class, 'store']);
