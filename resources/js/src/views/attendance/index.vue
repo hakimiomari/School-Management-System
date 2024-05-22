cecb403005e8b825a4bd4c940bbd0a8fdfce40cb
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-22 06:58:51 +0430

    onmount new function

--- resources/js/src/views/attendance/index.vue
+++ resources/js/src/views/attendance/index.vue
@@ -2,13 +2,14 @@
     <TakeAttendance :students="students" />
 </template>
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted } from "vue";
 import { useAttendance } from "@/composables/useAttendance";
 import TakeAttendance from "@/views/attendance/TakeAttendance.vue";
 
-const { index, students } = useAttendance();
+const { index, students, classDailyReport } = useAttendance();
 
 onMounted(() => {
     index();
+    classDailyReport();
 });
 </script>
