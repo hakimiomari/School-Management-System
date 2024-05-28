251d1701271027642ae2350ed08cb3736f5320b4
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-28 18:21:41 +0430

    edit and delete room

--- resources/js/src/views/Room.vue
+++ resources/js/src/views/Room.vue
@@ -180,18 +180,13 @@
                                 >
                                     <div class="flex items-center gap-2">
                                         <span
-                                            @click="
-                                                router.push({
-                                                    name: 'Editroom',
-                                                    params: { id: room.id },
-                                                })
-                                            "
+                                            @click="editRoom(room)"
                                             class="material-symbols-outlined text-emerald-600 cursor-pointer text-[20px]"
                                         >
                                             edit_square
                                         </span>
                                         <svg
-                                            @click="deleteFunc(room.id)"
+                                            @click="showDeleteAlert(room.id)"
                                             class="h-4 w-4 fill-red-600 cursor-pointer cursor-pointer"
                                             xmlns="http://www.w3.org/2000/svg"
                                             viewBox="0 0 448 512"
@@ -252,7 +247,7 @@
         </div>
     </div>
 
-    <!-- add new class -->
+    <!-- add new room -->
     <template>
         <TransitionRoot as="template" :show="open">
             <Dialog
@@ -466,6 +461,219 @@
             </Dialog>
         </TransitionRoot>
     </template>
+
+    <!-- Edit room -->
+    <template>
+        <TransitionRoot as="template" :show="appStore.showStatus">
+            <Dialog
+                as="div"
+                class="relative z-10"
+                @close="appStore.showStatus = false"
+            >
+                <TransitionChild
+                    as="template"
+                    enter="ease-out duration-300"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="ease-in duration-200"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                >
+                    <div
+                        class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+                    />
+                </TransitionChild>
+
+                <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+                    <div
+                        class="flex min-h-full items-center justify-center p-4 text-center sm:p-0"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="ease-out duration-300"
+                            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                            enter-to="opacity-100 translate-y-0 sm:scale-100"
+                            leave="ease-in duration-200"
+                            leave-from="opacity-100 translate-y-0 sm:scale-100"
+                            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        >
+                            <DialogPanel
+                                class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+                            >
+                                <div
+                                    class="bg-white w-full px-4 pb-4 pt-5 sm:p-6 sm:pb-4"
+                                >
+                                    <div class="sm:flex w-full sm:items-start">
+                                        <div
+                                            class="mt-3 w-full sm:ml-4 sm:mt-0 text-left"
+                                        >
+                                            <div class="flex justify-between">
+                                                <DialogTitle
+                                                    as="h3"
+                                                    class="text-base font-semibold leading-6 text-gray-900"
+                                                    >Edit Room</DialogTitle
+                                                >
+                                                <svg
+                                                    @click="
+                                                        appStore.showStatus = false
+                                                    "
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 352 512"
+                                                    class="w-5 h-5 fill-gray-600 cursor-pointer hover:fill-red-500"
+                                                >
+                                                    <path
+                                                        d="M242.7 256l100.1-100.1c12.3-12.3 12.3-32.2 0-44.5l-22.2-22.2c-12.3-12.3-32.2-12.3-44.5 0L176 189.3 75.9 89.2c-12.3-12.3-32.2-12.3-44.5 0L9.2 111.5c-12.3 12.3-12.3 32.2 0 44.5L109.3 256 9.2 356.1c-12.3 12.3-12.3 32.2 0 44.5l22.2 22.2c12.3 12.3 32.2 12.3 44.5 0L176 322.7l100.1 100.1c12.3 12.3 32.2 12.3 44.5 0l22.2-22.2c12.3-12.3 12.3-32.2 0-44.5L242.7 256z"
+                                                    />
+                                                </svg>
+                                            </div>
+                                            <div class="mt-2 w-full">
+                                                <form
+                                                    class="space-y-4"
+                                                    action="#"
+                                                    @submit.prevent="
+                                                        updateRoom(room)
+                                                    "
+                                                >
+                                                    <div class="w-full">
+                                                        <label
+                                                            class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                                            htmlfor="grid-password"
+                                                        >
+                                                            Name
+                                                        </label>
+                                                        <input
+                                                            type="text"
+                                                            class="bg-slate-100 border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5 dark:bg-slate-50 dark:border-gray-100 dark:placeholder-gray-400"
+                                                            placeholder="Physics Lab"
+                                                            required
+                                                            v-model="room.name"
+                                                        />
+                                                        <p
+                                                            v-if="errors?.name"
+                                                            class="text-xs mt-2 text-red-500"
+                                                        >
+                                                            {{
+                                                                errors?.name[0]
+                                                            }}
+                                                        </p>
+                                                    </div>
+                                                    <div class="w-full">
+                                                        <label
+                                                            for="number"
+                                                            class="block mb-2 text-sm font-medium text-gray-900"
+                                                            >Number</label
+                                                        >
+                                                        <input
+                                                            type="number"
+                                                            name="number"
+                                                            id="number"
+                                                            class="bg-slate-100 border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5 dark:bg-slate-50 dark:border-gray-100 dark:placeholder-gray-400"
+                                                            placeholder="102"
+                                                            required
+                                                            v-model="
+                                                                room.number
+                                                            "
+                                                        />
+                                                        <p
+                                                            v-if="
+                                                                errors?.number
+                                                            "
+                                                            class="text-xs mt-1 text-red-500"
+                                                        >
+                                                            {{
+                                                                errors
+                                                                    ?.number[0]
+                                                            }}
+                                                        </p>
+                                                    </div>
+                                                    <div class="w-full">
+                                                        <label
+                                                            class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                                                            for="capicity"
+                                                        >
+                                                            Capicity
+                                                        </label>
+                                                        <input
+                                                            type="number"
+                                                            name="capicity"
+                                                            id="capicity"
+                                                            class="bg-slate-100 border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5 dark:bg-slate-50 dark:border-gray-100 dark:placeholder-gray-400"
+                                                            placeholder="50"
+                                                            required
+                                                            v-model="
+                                                                room.capicity
+                                                            "
+                                                        />
+                                                        <p
+                                                            v-if="
+                                                                errors?.capicity
+                                                            "
+                                                            class="text-xs mt-2 text-red-500"
+                                                        >
+                                                            {{
+                                                                errors
+                                                                    ?.capicity[0]
+                                                            }}
+                                                        </p>
+                                                    </div>
+                                                    <div
+                                                        class="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6"
+                                                    >
+                                                        <button
+                                                            v-if="loading"
+                                                            type="button"
+                                                            :disabled="loading"
+                                                            class="inline-flex cursor-not-allowed items-center w-full justify-center rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 sm:ml-3 sm:w-auto"
+                                                        >
+                                                            <svg
+                                                                aria-hidden="true"
+                                                                role="status"
+                                                                class="inline w-4 h-4 me-3 text-white animate-spin"
+                                                                viewBox="0 0 100 101"
+                                                                fill="none"
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                            >
+                                                                <path
+                                                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                                    fill="#E5E7EB"
+                                                                />
+                                                                <path
+                                                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                                    fill="currentColor"
+                                                                />
+                                                            </svg>
+                                                            Updating
+                                                        </button>
+                                                        <button
+                                                            v-else
+                                                            type="submit"
+                                                            class="inline-flex w-full justify-center rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 sm:ml-3 sm:w-auto"
+                                                        >
+                                                            Update Room
+                                                        </button>
+                                                        <button
+                                                            type="button"
+                                                            class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-100 sm:mt-0 sm:w-auto"
+                                                            @click="
+                                                                appStore.showStatus = false
+                                                            "
+                                                            ref="cancelButtonRef"
+                                                        >
+                                                            Cancel
+                                                        </button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
+    </template>
 </template>
 
 <script setup>
@@ -476,6 +684,8 @@ import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Pagination from "@/components/Pagination.vue";
 import { useRoom } from "@/composables/useRoom";
+import Swal from "sweetalert2";
+import ProgressBar from "primevue/progressbar";
 
 import {
     Dialog,
@@ -485,7 +695,7 @@ import {
     TransitionRoot,
 } from "@headlessui/vue";
 
-const { store, errors } = useRoom();
+const { store, errors, deleteRoom, updateRoom } = useRoom();
 const products = ref(new Array(6));
 
 const appStore = useAppStore();
@@ -499,6 +709,27 @@ const roomData = ref({
     number: "",
     capicity: "",
 });
+const room = ref("");
+const editRoom = (data) => {
+    appStore.showStatus = true;
+    room.value = data;
+};
+
+const showDeleteAlert = (id) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+        if (result.isConfirmed) {
+            deleteRoom(id);
+        }
+    });
+};
 
 const page = JSON.parse(localStorage.getItem("pageName"));
 onBeforeMount(() => {
