948179da0bddd01b1f2ee7a3e70efff6acefd334
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-28 18:21:33 +0430

    edit and delete room

--- resources/js/src/composables/useRoom.js
+++ resources/js/src/composables/useRoom.js
@@ -11,6 +11,7 @@ export const useRoom = () => {
 
     const errors = ref("");
 
+    // adding room
     const store = async (roomData) => {
         token.value = getCookie("access_token");
         appStore.loading = true;
@@ -43,5 +44,70 @@ export const useRoom = () => {
             });
     };
 
-    return { store, errors };
+    // delete room
+
+    const deleteRoom = async (id) => {
+        token.value = getCookie("access_token");
+        await axios
+            .delete(`/api/school/room/delete/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${token.value}`,
+                },
+            })
+            .then((res) => {
+                appStore.onPageChange(appStore.current_page);
+                toast("Room successfully Deleted!", {
+                    theme: "auto",
+                    type: "success",
+                    autoClose: 1500,
+                    dangerouslyHTMLString: true,
+                });
+            })
+            .catch((err) => {
+                if (err.response.status == 422) {
+                    toast("Sorry! Room not found", {
+                        theme: "auto",
+                        type: "error",
+                        autoClose: 1500,
+                        dangerouslyHTMLString: true,
+                    });
+                }
+                if (err.response.status == 401) {
+                    removeCookie("access_token");
+                }
+            });
+    };
+
+    // update room
+
+    const updateRoom = async (data) => {
+        token.value = getCookie("access_token");
+        appStore.loading = true;
+        await axios
+            .patch("/api/school/room/update", data, {
+                headers: {
+                    Authorization: `Bearer ${token.value}`,
+                },
+            })
+            .then((res) => {
+                appStore.loading = false;
+                appStore.showStatus = false;
+                appStore.onPageChange(appStore.current_page);
+                toast("Room successfully Updated", {
+                    theme: "auto",
+                    type: "success",
+                    autoClose: 1500,
+                    dangerouslyHTMLString: true,
+                });
+            })
+            .catch((err) => {
+                appStore.loading = false;
+                errors.value = err.response.data.errors;
+                if (err.response.status == 401) {
+                    removeCookie("access_token");
+                }
+            });
+    };
+
+    return { store, errors, deleteRoom, updateRoom };
 };
