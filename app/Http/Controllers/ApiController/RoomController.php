ce2d4e12e33b36bf1d63795947b878285b9af78a
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-28 05:08:53 +0430

    add store function

--- app/Http/Controllers/ApiController/RoomController.php
+++ app/Http/Controllers/ApiController/RoomController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers\ApiController;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRoomRequest;
 use App\Models\Room;
 
 class RoomController extends Controller
 {
     public function index()
     {
-        $rooms = Room::paginate(10);
+        $rooms = Room::paginate(5);
         return response()->json($rooms);
+    }
+
+    // store room
+    public function store(StoreRoomRequest $request)
+    {
+        Room::create($request->all());
+        return response()->json(['message' => 'success'], 200);
     }
 }
