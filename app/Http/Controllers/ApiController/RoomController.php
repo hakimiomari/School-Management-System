2c35aae960f6e8131137c916bcbea6d745bf83a3
Kamran\kam <hakimikamranullah@gmail.com>
2024-05-28 18:21:09 +0430

    implement delete and edit room

--- app/Http/Controllers/ApiController/RoomController.php
+++ app/Http/Controllers/ApiController/RoomController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\ApiController;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRoomRequest;
+use App\Http\Requests\UpdateRoomRequest;
 use App\Models\Room;
+use Illuminate\Http\Request;
 
 class RoomController extends Controller
 {
@@ -18,6 +20,29 @@ class RoomController extends Controller
     public function store(StoreRoomRequest $request)
     {
         Room::create($request->all());
+        return response()->json(['message' => 'success'], 200);
+    }
+
+    // delete room
+    public function deleteRoom($id)
+    {
+        $room = Room::find($id);
+        if ($room) {
+            $room->delete();
+            return response()->json("success");
+        } else {
+            return response()->json(['message' => 'Room not found'], 422);
+        }
+    }
+
+    // update room
+    public function updateRoom(UpdateRoomRequest $request)
+    {
+        $room = Room::find($request->id);
+        $room->name = $request->name;
+        $room->number = $request->number;
+        $room->capicity = $request->capicity;
+        $room->save();
         return response()->json(['message' => 'success'], 200);
     }
 }
