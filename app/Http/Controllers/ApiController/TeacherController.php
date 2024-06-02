dd321bf01817a0a48bcdd8ae3e5c34f55c42b6ee
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-02 20:59:48 +0430

    teacher controller

--- app/Http/Controllers/ApiController/TeacherController.php
+++ app/Http/Controllers/ApiController/TeacherController.php
@@ -6,23 +6,43 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TeacherRegisterRequest;
 use App\Http\Requests\TeacherUpdateRequest;
 use App\Models\Teacher;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 
 class TeacherController extends Controller
 {
     public function index()
     {
-        $teachers = Teacher::paginate(10);
+        $teachers = Teacher::with('teacher')->paginate(10);
         return response()->json($teachers);
     }
 
     // store teacher
     public function store(TeacherRegisterRequest $request)
     {
-        $teacher = Teacher::create($request->all())->assignRole('Teacher');
-        $related_path = $request->photo->store('teacher_images', 'public');
-        $teacher->photo = $related_path;
-        $teacher->save();
+        $related_path = $request->photo->store('userImages', 'public');
+        $user_table = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'profession' => 'Teacher',
+            'bio' => $request->bio,
+            'password' => Hash::make('1234')
+        ];
+        $user = User::create($user_table)->assignRole('Teacher');
+        $user->image = $related_path;
+        $user->save();
+
+        $teacher_table = [
+            'user_id' => $user->id,
+            'father_name' => $request->father_name,
+            'degree' => $request->degree,
+            'date_of_birth' => $request->date_of_birth,
+            'gender' => $request->gender,
+            'address' => $request->address,
+            'contact' => $request->contact
+        ];
+        Teacher::create($teacher_table);
         return response()->json('success');
     }
 
@@ -30,9 +50,11 @@ class TeacherController extends Controller
     public function delete($id)
     {
         $teacher = Teacher::find($id);
-        if ($teacher->photo) {
-            Storage::disk('public')->delete($teacher->photo);
+        $user = User::find($teacher->user_id);
+        if ($user->image) {
+            Storage::disk('public')->delete($user->image);
         }
+        $user->delete();
         $teacher->delete();
         return response()->json('success');
     }
@@ -41,6 +63,7 @@ class TeacherController extends Controller
     public function getInfo($id)
     {
         $teacher = Teacher::find($id);
+        $teacher->teacher;
         return response()->json($teacher);
     }
     // get teacher info
@@ -55,18 +78,28 @@ class TeacherController extends Controller
     {
         $data = $request->all();
         $teacher = Teacher::find($request->id);
+        $user = User::find($request->user_id);
         $data = $request->except('photo');
         $data = $request->except('id');
+        $data = $request->except('bio');
+        $data = $request->except('email');
+        $data = $request->except('name');
         $teacher->update($data);
+
         if ($request->photo) {
-            $previousImagePath = $teacher->photo;
+            $previousImagePath = $user->image;
             if ($previousImagePath) {
                 Storage::disk('public')->delete($previousImagePath);
             }
-            $related_path = $request->photo->store('teacher_images', 'public');
-            $teacher->photo = $related_path;
-            $teacher->save();
+            $related_path = $request->photo->store('userImages', 'public');
+            $user->image = $related_path;
+            $user->save();
         }
+        $user->update([
+            'name' => $request->name,
+            'bio' => $request->bio,
+            'email' => $request->email
+        ]);
         return response()->json('sucesss');
     }
 }
