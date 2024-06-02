5cc0f1e87b4371d98f1507543407b076cfffc369
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-02 21:02:25 +0430

    teacher model updated

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory;
-    protected $fillable = ['father_name', 'degree', 'date_of_birth', 'gender', 'address', 'contact',];
+    protected $fillable = ['user_id', 'father_name', 'degree', 'date_of_birth', 'gender', 'address', 'contact',];
+
+
+    public function teacher()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
