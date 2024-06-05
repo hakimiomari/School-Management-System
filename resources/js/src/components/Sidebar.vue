0d4de4917414c93145e0bc6f8f8a0dda7921ecde
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-05 06:52:01 +0430

    add fees tab

--- resources/js/src/components/Sidebar.vue
+++ resources/js/src/components/Sidebar.vue
@@ -269,9 +269,9 @@ const inactiveClass = ref(
                     @click="isOpen = false"
                     class="flex items-center px-6 py-2 mt-4 duration-200 border-l-4"
                     :class="[
-                        route.name === 'Forms' ? activeClass : inactiveClass,
+                        route.name === 'Fees' ? activeClass : inactiveClass,
                     ]"
-                    to="/forms"
+                    to="/fees"
                 >
                     <svg
                         class="w-5 h-5"
@@ -288,7 +288,7 @@ const inactiveClass = ref(
                         />
                     </svg>
 
-                    <span class="mx-4">Schedule</span>
+                    <span class="mx-4">Fees</span>
                 </router-link>
 
                 <router-link
