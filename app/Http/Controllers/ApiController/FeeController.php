5747a43c4c69ee279f0238bea913f225854ffe20
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-06 22:56:42 +0430

    add controller

--- app/Http/Controllers/ApiController/FeeController.php
+++ app/Http/Controllers/ApiController/FeeController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use App\Services\Service;
 use Illuminate\Http\Request;
 
 class FeeController extends Controller
 {
-    //
+    private $services;
+
+    public function __construct(Service $services)
+    {
+        $this->services = $services;
+    }
+
 }
