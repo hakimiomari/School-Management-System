ba3fa6e15247c0c93e3889103aeec99978e480f4
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-06 22:57:13 +0430

    create services

--- app/Services/Service.php
+++ app/Services/Service.php
@@ -2,6 +2,15 @@
 
 namespace App\Services;
 
+use App\Models\Classes;
+use App\Models\Fee;
+use App\Models\Student;
+use Carbon\Carbon;
+
 class Service
 {
+    public function runMonthlyForFees()
+    {
+
+        
 }
