79f4ab40ee925321298e8be986dfd2feb9b739a2
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-07 14:16:01 +0430

    edit fees model

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -8,6 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Fee extends Model
 {
     use HasFactory;
+    protected $fillable = ['student_id', 'class_id', 'fee', 'payed', 'remain'];
 
-    protected $fillable = ['student_id', 'class_id', 'year', 'months', 'fee', 'payed', 'remaind'];
+    public function studentInfo()
+    {
+        return $this->hasOne(Student::class, 'id', 'student_id');
+    }
 }
