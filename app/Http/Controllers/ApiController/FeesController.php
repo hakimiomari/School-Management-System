c952db5b9cbf6f84b7734de6a6ad7401243f518d
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-07 19:29:54 +0430

    update fees controller

--- app/Http/Controllers/ApiController/FeesController.php
+++ app/Http/Controllers/ApiController/FeesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\ApiController;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GetFeesRequest;
+use App\Http\Requests\TakeFeesRequest;
+use App\Http\Requests\TakeRemainRequest;
 use App\Models\Fee;
 use Carbon\Carbon;
 
@@ -17,5 +19,40 @@ class FeesController extends Controller
         $students = Fee::with('studentInfo')->where('class_id', $request->class_id)->WhereYear('created_at', $year)->whereMonth('created_at', $month)->get();
 
         return response()->json($students);
+    }
+
+    // /takeFees
+    public function takeFees(TakeFeesRequest $request)
+    {
+        $fee = Fee::where('student_id', $request->id)->first();
+        if ($request->fee <= $fee->fee) {
+            $remain = $fee->remain  - $request->fee;
+            $fee->update([
+                'payed' => $request->fee,
+                'remain' => $remain
+            ]);
+        } else {
+            $fee = (string)$fee->fee;
+            return response()->json(['errors' => ['fee' => ['The Fee Should be less than or equal to ' . $fee]]], 422);
+        }
+        return response()->json('success');
+    }
+
+    // takeRemain
+    public function takeRemain(TakeRemainRequest $request)
+    {
+        $fee = Fee::where('student_id', $request->id)->first();
+        if ($request->remain <= $fee->remain) {
+            $remain = $fee->remain  - $request->remain;
+            $payed = $fee->payed + $request->remain;
+            $fee->update([
+                'payed' => $payed,
+                'remain' => $remain
+            ]);
+        } else {
+            $remain = (string)$fee->remain;
+            return response()->json(['errors' => ['remain' => ['The Remain Should be less than or equal to ' . $remain]]], 422);
+        }
+        return response()->json('success');
     }
 }
