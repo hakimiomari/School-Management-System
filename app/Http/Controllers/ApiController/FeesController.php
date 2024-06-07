e208016320dc3505a7d3a5e3dedb8cd900a2fa9b
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-07 14:15:24 +0430

    retrieve student data

--- app/Http/Controllers/ApiController/FeesController.php
+++ app/Http/Controllers/ApiController/FeesController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\GetFeesRequest;
 use App\Models\Fee;
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class FeesController extends Controller
 {
     public function getClassMonthlyInfo(GetFeesRequest $request)
     {
-        $students = Fee::where('class_id', $request->class_id)->where('months', $request->month)->get();
+        $carbon = Carbon::parse($request->date);
+        $month = $carbon->format('m');
+        $year = $carbon->format('Y');
+        $students = Fee::with('studentInfo')->where('class_id', $request->class_id)->WhereYear('created_at', $year)->whereMonth('created_at', $month)->get();
 
         return response()->json($students);
     }
