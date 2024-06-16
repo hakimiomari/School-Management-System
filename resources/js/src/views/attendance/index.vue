028e6ae93ad0c98449559062d7721456812f20fd
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-16 22:31:18 +0430

    add new features

--- resources/js/src/views/attendance/index.vue
+++ resources/js/src/views/attendance/index.vue
@@ -1,15 +1,7 @@
 <template>
-    <TakeAttendance :students="students" />
+    <TakeAttendance  />
 </template>
 <script setup>
-import { onMounted } from "vue";
-import { useAttendance } from "@/composables/useAttendance";
 import TakeAttendance from "@/views/attendance/TakeAttendance.vue";
 
-const { index, students, classDailyReport } = useAttendance();
-
-onMounted(() => {
-    index();
-    classDailyReport();
-});
 </script>
