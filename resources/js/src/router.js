34de63636e39ccc04be3c7560d44580bc0deb5b6
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-21 15:40:33 +0430

    remove unessential codes and files

--- resources/js/src/router.js
+++ resources/js/src/router.js
@@ -4,10 +4,8 @@ import { useLogin } from "@/composables/user/useLogin";
 import { useUser } from "@/composables/user/useUser";
 import { userStore } from "@/store/userStore";
 import Dashboard from "@/views/Dashboard.vue";
-import Tables from "@/views/Tables.vue";
 import UIElements from "@/views/UIElements.vue";
 import Login from "@/views/Login.vue";
-import Modal from "@/views/Modal.vue";
 import Room from "@/views/Room.vue";
 import Blank from "@/views/Blank.vue";
 import Profile from "@/views/profile/Profile.vue";
@@ -132,19 +130,9 @@ const routes = [
         meta: { isAuth: true, role: "Admin" },
     },
     {
-        path: "/tables",
-        name: "Tables",
-        component: Tables,
-    },
-    {
         path: "/ui-elements",
         name: "UIElements",
         component: UIElements,
-    },
-    {
-        path: "/modal",
-        name: "Modal",
-        component: Modal,
     },
     {
         path: "/blank",
