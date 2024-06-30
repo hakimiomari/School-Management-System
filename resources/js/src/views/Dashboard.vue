b7864a24d1a23ad17bbb10c399b57538d69db2db
Kamran\kam <hakimikamranullah@gmail.com>
2024-06-30 21:54:21 +0430

    dashboard

--- resources/js/src/views/Dashboard.vue
+++ resources/js/src/views/Dashboard.vue
@@ -2,8 +2,7 @@
 import { ref, onMounted } from "vue";
 import { useApp } from "@/composables/useApp";
 import { useAppStore } from "@/store/useAppStore";
-import Chart from 'primevue/chart';
-
+import Chart from "primevue/chart";
 
 const appStore = useAppStore();
 const {
@@ -14,11 +13,82 @@ const {
     obsentStudents,
     revenue,
     remain,
+    getMonthlyFeeReport
 } = useApp();
+
+const chartData = ref();
+const chartOptions = ref();
 
 onMounted(() => {
     getDataForDashboard();
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+    getMonthlyFeeReport();
 });
+
+const setChartData = () => {
+    return {
+        labels: ["Q1", "Q2", "Q3", "Q4"],
+        datasets: [
+            {
+                label: "Fee",
+                data: [540, 325, 702, 620],
+                backgroundColor: [
+                    "rgba(249, 115, 22, 0.2)",
+                    "rgba(6, 182, 212, 0.2)",
+                    "rgb(107, 114, 128, 0.2)",
+                    "rgba(139, 92, 246 0.2)",
+                ],
+                borderColor: [
+                    "rgb(249, 115, 22)",
+                    "rgb(6, 182, 212)",
+                    "rgb(107, 114, 128)",
+                    "rgb(139, 92, 246)",
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+};
+const setChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue("--p-text-color");
+    const textColorSecondary = documentStyle.getPropertyValue(
+        "--p-text-muted-color"
+    );
+    const surfaceBorder = documentStyle.getPropertyValue(
+        "--p-content-border-color"
+    );
+
+    return {
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor,
+                },
+            },
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary,
+                },
+                grid: {
+                    color: surfaceBorder,
+                },
+            },
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    color: textColorSecondary,
+                },
+                grid: {
+                    color: surfaceBorder,
+                },
+            },
+        },
+    };
+};
 </script>
 
 <template>
@@ -410,17 +480,18 @@ onMounted(() => {
                 </div>
             </div>
         </div>
-        <div class="mt-4">
-            <h1 class="text-3xl">
-                Attendence Report
-            </h1>
-            <div>
-
+        <div class="mt-4 flex report">
+            <h1 class="text-3xl">Attendence Report</h1>
+            <div class="w-2/3 mt-6">
+                <Chart type="bar" :data="chartData" :options="chartOptions" />
             </div>
         </div>
     </div>
 </template>
 <style scoped>
+.report {
+    flex-direction: column;
+}
 .card {
     height: 100px;
     width: 300px;
